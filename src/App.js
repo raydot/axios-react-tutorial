d765bbf44404abda6b4012d8b69cf46e094b596c
Dave Kanter <moatgoat@hotmail.com>
2019-06-06 20:49:45 -0700

    First part of the tutorial completed.  Stopped at the part where I had to figure out connecting it to the mockable API.

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import API from './utils/API'
+import User from './User'
+
+class App extends React.Component {
+  // render() {
+  //   return <User name='Cornellius Kaye' avatar='https://randomuser.me/api/portraits/men/16.jpg' email='hello@jessica.com' />
+  // }
+  constructor(props) {
+    super(props)
+
+    /*
+      Give the component its own state.  This will help with keeping track of
+      the loading state, the person's name, avatar, and email.  It's also using
+      state data to hydrate the User component when the component is re-rendered.
+    */
+
+    this.state = {
+      isLoading: true,
+      name: null,
+      avatar: null,
+      email:null
+    }
+  } // constructor
+
+  render() {
+    const {isLoading, name, avatar, email} = this.state
+
+    return (
+      <User isLoading={isLoading} name={name} avatar={avatar} email={email} />
+    )
+  } // render
+
+  /*
+    This comes complete with an async request to load the data and update the
+    component state.  That will trigger a new re-render.  Notice that the method
+    is async which will allow us to await certain actions inside.
+  */
+
+  async componentDidMount() {
+    // Load async data
+    let userData = await API.get('/', {
+      params: {
+        results: 1,
+        inc: 'name, email, picture'
+      }
+    })
+
+    // Parse the results for ease of use
+    userData = userData.data.results[0]
+
+    // Update state with new data
+    const name = `${userData.name.first} ${userData.name.last}`
+    const avatar = userData.picture.large
+    const email = userData.email
+    // re-render this ccomponent
+    this.setState({
+      ...this.state, ...{
+        isLoading: false,
+        name,
+        avatar,
+        email
+      }
+    })
+  }
+}
+
+export default App;
